7bd69c05f2ff10df8a507fc89eb7ab2e4ac38a54
Łukasz Fedorowiat <lukaszfedorowiat@gmail.com>
2026-05-22 07:53:36 +0200

    fix test

--- connect/lib/connectors/highlevel_client_test.go
+++ connect/lib/connectors/highlevel_client_test.go
@@ -1,497 +1,333 @@
+//go:build !integration
+
 package connectors
 
 import (
-	"crypto/tls"
-	"fmt"
-	"sync"
+	"testing"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-// HighLevelClient support all Kafka Connect API functions + helper features.
-type HighLevelClient interface {
-	// Kafka Connect API
-	GetAll() (GetAllConnectorsResponse, error)
-	GetConnector(req ConnectorRequest) (ConnectorResponse, error)
-
-	CreateConnector(
-		req CreateConnectorRequest,
-		sync bool,
-		timeoutOptional ...time.Duration,
-	) (ConnectorResponse, error)
-
-	UpdateConnector(
-		req CreateConnectorRequest,
-		sync bool,
-		timeoutOptional ...time.Duration,
-	) (ConnectorResponse, error)
-
-	DeleteConnector(
-		req ConnectorRequest,
-		sync bool,
-		timeoutOptional ...time.Duration,
-	) (EmptyResponse, error)
-
-	GetConnectorConfig(req ConnectorRequest) (GetConnectorConfigResponse, error)
-	GetConnectorStatus(req ConnectorRequest) (GetConnectorStatusResponse, error)
-	RestartConnector(req ConnectorRequest) (EmptyResponse, error)
-
-	PauseConnector(
-		req ConnectorRequest,
-		sync bool,
-		timeoutOptional ...time.Duration,
-	) (EmptyResponse, error)
-
-	ResumeConnector(
-		req ConnectorRequest,
-		sync bool,
-		timeoutOptional ...time.Duration,
-	) (EmptyResponse, error)
-
-	GetAllTasks(req ConnectorRequest) (GetAllTasksResponse, error)
-	GetTaskStatus(req TaskRequest) (TaskStatusResponse, error)
-	RestartTask(req TaskRequest) (EmptyResponse, error)
-
-	// helper methods
-	IsUpToDate(
-		connector string,
-		config map[string]interface{},
-	) (bool, error)
-
-	DeployConnector(
-		req CreateConnectorRequest,
-		timeoutOptional ...time.Duration,
-	) error
-
-	DeployMultipleConnector(
-		connectors []CreateConnectorRequest,
-		timeoutOptional ...time.Duration,
-	) error
-
-	SetInsecureSSL()
-	SetDebug()
-	SetClientCertificates(certs ...tls.Certificate)
-	SetParallelism(value int)
-	SetBasicAuth(username string, password string)
-	SetHeader(name string, value string)
-}
-
-type highLevelClient struct {
-	client             BaseClient
-	maxParallelRequest int
-}
-
-// NewClient creates a new HighLevelClient.
-func NewClient(
-	url string,
-	timeoutOptional ...time.Duration,
-) HighLevelClient {
-
-	timeout := 10 * time.Second
-
-	if len(timeoutOptional) > 0 {
-		timeout = timeoutOptional[0]
+func Test_IsUpToDate_Should_Be_True(t *testing.T) {
+	configOnline := map[string]interface{}{
+		"name":   "test1",
+		"param1": 2,
+		"param2": "abc",
+		"param3": "3",
 	}
 
-	return &highLevelClient{
-		client:             newBaseClient(url, timeout),
-		maxParallelRequest: 3,
-	}
-}
-
-// -----------------------------------------------------------------------------
-// Config
-// -----------------------------------------------------------------------------
-
-func (c *highLevelClient) SetParallelism(value int) {
-	c.maxParallelRequest = value
-}
-
-func (c *highLevelClient) SetInsecureSSL() {
-	c.client.SetInsecureSSL()
-}
-
-func (c *highLevelClient) SetDebug() {
-	c.client.SetDebug()
-}
-
-func (c *highLevelClient) SetClientCertificates(certs ...tls.Certificate) {
-	c.client.SetClientCertificates(certs...)
-}
-
-func (c *highLevelClient) SetBasicAuth(username string, password string) {
-	c.client.SetBasicAuth(username, password)
-}
-
-func (c *highLevelClient) SetHeader(name string, value string) {
-	c.client.SetHeader(name, value)
-}
-
-// -----------------------------------------------------------------------------
-// Connectors
-// -----------------------------------------------------------------------------
-
-func (c *highLevelClient) GetAll() (GetAllConnectorsResponse, error) {
-	return c.client.GetAll()
-}
-
-func (c *highLevelClient) GetConnector(
-	req ConnectorRequest,
-) (ConnectorResponse, error) {
-	return c.client.GetConnector(req)
-}
-
-func (c *highLevelClient) CreateConnector(
-	req CreateConnectorRequest,
-	sync bool,
-	timeoutOptional ...time.Duration,
-) (ConnectorResponse, error) {
-
-	result, err := c.client.CreateConnector(req)
-	if err != nil {
-		return result, err
+	configLocal := map[string]interface{}{
+		"param1": 2,
+		"param2": "abc",
+		"param3": 3,
 	}
 
-	if sync {
-		timeout := resolveTimeout(timeoutOptional...)
+	mockBaseClient := &MockBaseClient{}
 
-		if !tryUntil(
-			func() bool {
-				resp, err := c.GetConnector(req.ConnectorRequest)
-				return err == nil && resp.Code == 200
-			},
-			timeout,
-		) {
-			return result, errors.New(
-				"timeout waiting for connector creation",
-			)
-		}
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(GetConnectorConfigResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Config:        configOnline,
+		}, nil)
+
+	client := &highLevelClient{
+		client: mockBaseClient,
 	}
 
-	return result, nil
+	isUpToDate, err := client.IsUpToDate("test1", configLocal)
+
+	assert.NoError(t, err)
+	assert.True(t, isUpToDate)
 }
 
-func (c *highLevelClient) UpdateConnector(
-	req CreateConnectorRequest,
-	sync bool,
-	timeoutOptional ...time.Duration,
-) (ConnectorResponse, error) {
-
-	result, err := c.client.UpdateConnector(req)
-	if err != nil {
-		return result, err
+func Test_IsUpToDate_Should_Be_False(t *testing.T) {
+	configOnline := map[string]interface{}{
+		"name":   "test1",
+		"param1": 3,
+		"param2": "abc",
+		"param3": "3",
 	}
 
-	if sync {
-		timeout := resolveTimeout(timeoutOptional...)
-
-		if !tryUntil(
-			func() bool {
-				upToDate, err := c.IsUpToDate(req.Name, req.Config)
-				return err == nil && upToDate
-			},
-			timeout,
-		) {
-			return result, errors.New(
-				"timeout waiting for connector update",
-			)
-		}
+	configLocal := map[string]interface{}{
+		"param1": 2,
+		"param2": "abc",
+		"param3": 3,
 	}
 
-	return result, nil
+	mockBaseClient := &MockBaseClient{}
+
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(GetConnectorConfigResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Config:        configOnline,
+		}, nil)
+
+	client := &highLevelClient{
+		client: mockBaseClient,
+	}
+
+	isUpToDate, err := client.IsUpToDate("test1", configLocal)
+
+	assert.NoError(t, err)
+	assert.False(t, isUpToDate)
 }
 
-func (c *highLevelClient) DeleteConnector(
-	req ConnectorRequest,
-	sync bool,
-	timeoutOptional ...time.Duration,
-) (EmptyResponse, error) {
-
-	result, err := c.client.DeleteConnector(req)
-	if err != nil {
-		return result, err
-	}
-
-	if sync {
-		timeout := resolveTimeout(timeoutOptional...)
-
-		if !tryUntil(
-			func() bool {
-				resp, err := c.GetConnector(req)
-				return err == nil && resp.Code == 404
-			},
-			timeout,
-		) {
-			return result, errors.New(
-				"timeout waiting for connector deletion",
-			)
-		}
-	}
-
-	return result, nil
-}
-
-func (c *highLevelClient) GetConnectorConfig(
-	req ConnectorRequest,
-) (GetConnectorConfigResponse, error) {
-	return c.client.GetConnectorConfig(req)
-}
-
-func (c *highLevelClient) GetConnectorStatus(
-	req ConnectorRequest,
-) (GetConnectorStatusResponse, error) {
-	return c.client.GetConnectorStatus(req)
-}
-
-func (c *highLevelClient) RestartConnector(
-	req ConnectorRequest,
-) (EmptyResponse, error) {
-	return c.client.RestartConnector(req)
-}
-
-func (c *highLevelClient) PauseConnector(
-	req ConnectorRequest,
-	sync bool,
-	timeoutOptional ...time.Duration,
-) (EmptyResponse, error) {
-
-	result, err := c.client.PauseConnector(req)
-	if err != nil {
-		return result, err
-	}
-
-	if sync {
-		timeout := resolveTimeout(timeoutOptional...)
-
-		if !tryUntil(
-			func() bool {
-				resp, err := c.GetConnectorStatus(req)
-
-				return err == nil &&
-					resp.Code == 200 &&
-					resp.ConnectorStatus["state"] == "PAUSED"
-			},
-			timeout,
-		) {
-			return result, errors.New(
-				"timeout waiting for connector pause",
-			)
-		}
-	}
-
-	return result, nil
-}
-
-func (c *highLevelClient) ResumeConnector(
-	req ConnectorRequest,
-	sync bool,
-	timeoutOptional ...time.Duration,
-) (EmptyResponse, error) {
-
-	result, err := c.client.ResumeConnector(req)
-	if err != nil {
-		return result, err
-	}
-
-	if sync {
-		timeout := resolveTimeout(timeoutOptional...)
-
-		if !tryUntil(
-			func() bool {
-				resp, err := c.GetConnectorStatus(req)
-
-				return err == nil &&
-					resp.Code == 200 &&
-					resp.ConnectorStatus["state"] == "RUNNING"
-			},
-			timeout,
-		) {
-			return result, errors.New(
-				"timeout waiting for connector resume",
-			)
-		}
-	}
-
-	return result, nil
-}
-
-// -----------------------------------------------------------------------------
-// Helpers
-// -----------------------------------------------------------------------------
-
-func resolveTimeout(timeoutOptional ...time.Duration) time.Duration {
-	timeout := 2 * time.Minute
-
-	if len(timeoutOptional) > 0 {
-		timeout = timeoutOptional[0]
-	}
-
-	return timeout
-}
-
-func (c *highLevelClient) IsUpToDate(
-	connector string,
-	config map[string]interface{},
-) (bool, error) {
-
-	copyConfig := make(map[string]interface{}, len(config))
-
-	for key, value := range config {
-		copyConfig[key] = value
-	}
-
-	copyConfig["name"] = connector
-
-	configResp, err := c.GetConnectorConfig(
-		ConnectorRequest{Name: connector},
-	)
-	if err != nil {
-		return false, err
-	}
-
-	if configResp.Code == 404 {
-		return false, nil
-	}
-
-	if configResp.Code >= 400 {
-		return false, errors.New(
-			fmt.Sprintf("status code: %d", configResp.Code),
-		)
-	}
-
-	if len(configResp.Config) != len(copyConfig) {
-		return false, nil
-	}
-
-	for key, value := range configResp.Config {
-		if convertConfigValueToString(copyConfig[key]) !=
-			convertConfigValueToString(value) {
-			return false, nil
-		}
-	}
-
-	return true, nil
-}
-
-func convertConfigValueToString(value interface{}) string {
-	return fmt.Sprintf("%v", value)
-}
-
-func tryUntil(exec func() bool, limit time.Duration) bool {
-	timeout := time.NewTimer(limit)
-	defer timeout.Stop()
-
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		if exec() {
+func Test_tryUntil_When_Success(t *testing.T) {
+	result := tryUntil(
+		func() bool {
 			return true
-		}
-
-		select {
-		case <-timeout.C:
-			return false
-
-		case <-ticker.C:
-			continue
-		}
-	}
-}
-
-func (c *highLevelClient) DeployConnector(
-	req CreateConnectorRequest,
-	timeoutOptional ...time.Duration,
-) error {
-
-	existingConnector, err := c.GetConnector(
-		ConnectorRequest{Name: req.Name},
+		},
+		100*time.Millisecond,
 	)
-	if err != nil {
-		return err
-	}
 
-	if existingConnector.Code != 404 {
-		upToDate, err := c.IsUpToDate(req.Name, req.Config)
-		if err != nil {
-			return err
-		}
-
-		if upToDate {
-			return nil
-		}
-	}
-
-	_, err = c.UpdateConnector(req, true, timeoutOptional...)
-
-	return err
+	assert.True(t, result)
 }
 
-func (c *highLevelClient) DeployMultipleConnector(
-	connectors []CreateConnectorRequest,
-	timeoutOptional ...time.Duration,
-) (err error) {
+func Test_tryUntil_When_Timeout(t *testing.T) {
+	result := tryUntil(
+		func() bool {
+			time.Sleep(200 * time.Millisecond)
+			return true
+		},
+		100*time.Millisecond,
+	)
 
-	errSync := new(sync.Mutex)
+	assert.False(t, result)
+}
 
-	throttleCh := make(chan interface{}, c.maxParallelRequest)
+func Test_DeployConnector_When_Already_Up_To_Date(t *testing.T) {
+	configOnline := map[string]interface{}{
+		"name":   "test1",
+		"param1": 2,
+		"param2": "abc",
+		"param3": "3",
+	}
 
-	for _, connector := range connectors {
-		throttleCh <- struct{}{}
+	configLocal := map[string]interface{}{
+		"param1": 2,
+		"param2": "abc",
+		"param3": 3,
+	}
 
-		go func(req CreateConnectorRequest) {
-			defer func() {
-				<-throttleCh
-			}()
+	mockBaseClient := &MockBaseClient{}
 
-			newErr := c.DeployConnector(
-				req,
-				timeoutOptional...,
-			)
+	mockBaseClient.On("GetConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Name:          "test1",
+			Config:        configOnline,
+		}, nil)
 
-			if newErr != nil {
-				errSync.Lock()
-				defer errSync.Unlock()
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(GetConnectorConfigResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Config:        configOnline,
+		}, nil)
 
-				err = multierror.Append(
-					err,
-					errors.Wrapf(
-						newErr,
-						"error while deploying: %v",
-						req.Name,
-					),
-				)
+	client := &highLevelClient{
+		client: mockBaseClient,
+	}
+
+	err := client.DeployConnector(
+		CreateConnectorRequest{
+			ConnectorRequest: ConnectorRequest{
+				Name: "test1",
+			},
+			Config: configLocal,
+		},
+	)
+
+	assert.NoError(t, err)
+
+	mockBaseClient.AssertExpectations(t)
+	mockBaseClient.AssertNotCalled(t, "UpdateConnector", mock.Anything)
+}
+
+func Test_DeployConnector_Ok(t *testing.T) {
+	configOnline := map[string]interface{}{
+		"name":   "test1",
+		"param1": 2,
+	}
+
+	configLocal := map[string]interface{}{
+		"param1": 3,
+	}
+
+	updatedConfig := map[string]interface{}{
+		"name":   "test1",
+		"param1": 3,
+	}
+
+	mockBaseClient := &MockBaseClient{}
+
+	mockBaseClient.On("GetConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Name:          "test1",
+			Config:        configOnline,
+		}, nil)
+
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(GetConnectorConfigResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Config:        configOnline,
+		}, nil).Once()
+
+	mockBaseClient.On("UpdateConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Name:          "test1",
+			Config:        updatedConfig,
+		}, nil)
+
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(GetConnectorConfigResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+			Config:        updatedConfig,
+		}, nil).Once()
+
+	client := &highLevelClient{
+		client: mockBaseClient,
+	}
+
+	err := client.DeployConnector(
+		CreateConnectorRequest{
+			ConnectorRequest: ConnectorRequest{
+				Name: "test1",
+			},
+			Config: configLocal,
+		},
+		1*time.Second,
+	)
+
+	assert.NoError(t, err)
+
+	mockBaseClient.AssertExpectations(t)
+}
+
+func Test_DeployMultipleConnector_Ok(t *testing.T) {
+	mockBaseClient := &MockBaseClient{}
+
+	mockBaseClient.On("GetConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 404},
+		}, nil)
+
+	mockBaseClient.On("UpdateConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 200},
+		}, nil)
+
+	mockBaseClient.On("GetConnectorConfig", mock.Anything).
+		Return(func(req ConnectorRequest) GetConnectorConfigResponse {
+			return GetConnectorConfigResponse{
+				EmptyResponse: EmptyResponse{Code: 200},
+				Config: map[string]interface{}{
+					"name": req.Name,
+				},
 			}
-		}(connector)
+		}, nil)
+
+	client := &highLevelClient{
+		client:             mockBaseClient,
+		maxParallelRequest: 2,
 	}
 
-	for i := 0; i < c.maxParallelRequest; i++ {
-		throttleCh <- struct{}{}
+	err := client.DeployMultipleConnector(
+		[]CreateConnectorRequest{
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test1",
+				},
+				Config: map[string]interface{}{
+					"name": "test1",
+				},
+			},
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test2",
+				},
+				Config: map[string]interface{}{
+					"name": "test2",
+				},
+			},
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test3",
+				},
+				Config: map[string]interface{}{
+					"name": "test3",
+				},
+			},
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test4",
+				},
+				Config: map[string]interface{}{
+					"name": "test4",
+				},
+			},
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test5",
+				},
+				Config: map[string]interface{}{
+					"name": "test5",
+				},
+			},
+		},
+		1*time.Second,
+	)
+
+	assert.NoError(t, err)
+
+	mockBaseClient.AssertNumberOfCalls(
+		t,
+		"UpdateConnector",
+		5,
+	)
+}
+
+func Test_DeployMultipleConnector_Error(t *testing.T) {
+	mockBaseClient := &MockBaseClient{}
+
+	mockBaseClient.On("GetConnector", mock.Anything).
+		Return(ConnectorResponse{
+			EmptyResponse: EmptyResponse{Code: 404},
+		}, nil)
+
+	mockBaseClient.On("UpdateConnector", mock.Anything).
+		Return(ConnectorResponse{}, errors.New("random error"))
+
+	client := &highLevelClient{
+		client:             mockBaseClient,
+		maxParallelRequest: 2,
 	}
 
-	return err
-}
+	err := client.DeployMultipleConnector(
+		[]CreateConnectorRequest{
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test1",
+				},
+				Config: map[string]interface{}{
+					"name": "test1",
+				},
+			},
+			{
+				ConnectorRequest: ConnectorRequest{
+					Name: "test2",
+				},
+				Config: map[string]interface{}{
+					"name": "test2",
+				},
+			},
+		},
+		1*time.Second,
+	)
 
-// -----------------------------------------------------------------------------
-// Tasks
-// -----------------------------------------------------------------------------
-
-func (c *highLevelClient) GetAllTasks(
-	req ConnectorRequest,
-) (GetAllTasksResponse, error) {
-	return c.client.GetAllTasks(req)
-}
-
-func (c *highLevelClient) GetTaskStatus(
-	req TaskRequest,
-) (TaskStatusResponse, error) {
-	return c.client.GetTaskStatus(req)
-}
-
-func (c *highLevelClient) RestartTask(
-	req TaskRequest,
-) (EmptyResponse, error) {
-	return c.client.RestartTask(req)
+	assert.Error(t, err)
 }
